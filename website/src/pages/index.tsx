5d29175bfe621e2998404429be4456378c5fdd57
Debsify Satisfying <debsify@outlook.com>
2024-11-01 14:43:19 +0000

    perbaikan lanjutan

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -4,40 +4,80 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
+import Translate, {translate} from '@docusaurus/Translate';
 
 import styles from './index.module.css';
-import {Button} from '@mui/material';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+function HeroBanner() {
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="maintitle">
-          <h1>Powerful ♥️</h1>
+    <div className={styles.hero} data-theme="dark">
+      <div className={styles.heroInner}>
+        <Heading as="h1" className={styles.heroProjectTagline}>
+        <img
+            alt={translate({message: 'Docusaurus with Keytar'})}
+            className={styles.heroLogo}
+            src={('/img/logo.svg')}
+            width="200"
+            height="200"
+          />
+          <span
+            className={styles.heroTitleTextHtml}
+            dangerouslySetInnerHTML={{
+              __html: translate({
+                id: 'homepage.hero.title',
+                message:
+                  'Build <b>optimized</b> websites <b>quickly</b>, focus on your <b>content</b>',
+                description:
+                  'Home page hero title, can contain simple html tags',
+              }),
+            }}
+          />
         </Heading>
-        <p className="subtitle"> {siteConfig.tagline} </p>
-        <div className={styles.buttons}>
-          <Link className="button bui1" to="docs/intro">
-            <Button variant="contained">Document</Button>
+        <div className={styles.indexCtas}>
+          <Link className="button button--primary" to="/docs">
+            <Translate>Get Started</Translate>
           </Link>
-          <Link className="button bui2" to="blog">
-            <Button variant="contained">Articles</Button>
+          <Link className="button button--info" to="https://docusaurus.new">
+            <Translate>Try a Demo</Translate>
           </Link>
         </div>
       </div>
-    </header>
+   </div>
   );
 }
 
-export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+function TopBanner() {
+  const announcedVersion = useDocusaurusContext().siteConfig.customFields
+    ?.announcedVersion as string;
   return (
-    <Layout
-      title= {siteConfig.title}
-      description='deskipsi'>
-      <HomepageHeader />
+    <div className={styles.topBanner}>
+      <div className={styles.topBannerTitle}>
+        {'🎉\xa0'}
+        <Link
+          to={`/blog/releases/${announcedVersion}`}
+          className={styles.topBannerTitleText}>
+          <Translate
+            id="homepage.banner.launch.newVersion"
+            values={{new: announcedVersion}}>
+            {'Docusaurus\xa03.5 is\xa0out!️'}
+          </Translate>
+        </Link>
+        {'\xa0🥳'}
+      </div>
+      </div>
+  );
+}
+  
+export default function Home(): JSX.Element {
+  const {
+    siteConfig: {customFields, tagline},
+  } = useDocusaurusContext();
+  const {description} = customFields as {description: string};
+  return (
+    <Layout title={tagline} description={description}>
       <main>
+      <TopBanner />
+      <HeroBanner />
       <HomepageFeatures />
       </main>
     </Layout>
